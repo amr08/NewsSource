0b44a0dc4320596b61f20266595c20a7e1775c8e
Andrea R <amrdev08@gmail.com>
2016-12-10 19:52:12 -0500

    staging err

--- db/db.js
+++ db/db.js
@@ -4,7 +4,14 @@
 
   mongoose.Promise = Promise;
 
-  mongoose.connect("mongodb://heroku_h1ttmm5z:bg69nfsalu9s08df3u6h8pcfeo@ds115738.mlab.com:15738/heroku_h1ttmm5z");
+  var databaseUri = "mongodb://heroku_h1ttmm5z:bg69nfsalu9s08df3u6h8pcfeo@ds115738.mlab.com:15738/heroku_h1ttmm5z";
+
+  if(process.env.MONGODB_URI) {
+    mongoose.connect(process.env.MONGODB_URI);
+  } else {
+    mongoose.connect(databaseUri);
+  }
+  
   var db = mongoose.connection;
 
   db.on("error", function(error) {
